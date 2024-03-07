75566231c258a603b3cde60147a76500c8b9dd6d
temcryptodev <info@temcrypto.com>
2024-03-07 20:00:26 -0300

    update DB files

--- app/api/rates/[pair]/route.ts
+++ app/api/rates/[pair]/route.ts
@@ -1,6 +1,7 @@
 import { type NextRequest, NextResponse } from 'next/server';
 
-import { getUsers } from '@/lib/db/getUsers';
+import { db } from '@/lib/db';
+import { users } from '@/lib/db/schema';
 import getRate from '@/lib/kp/getRate';
 import { type kpRatePair } from '@/lib/kp/types';
 
@@ -12,9 +13,12 @@ export async function GET(
 
   try {
     const rate = await getRate(pair, 'charge', 100);
-    const users = await getUsers();
+    const usersResult = await db.select().from(users);
 
-    return NextResponse.json({ pair, rate, users }, { status: 200 });
+    return NextResponse.json(
+      { pair, rate, users: usersResult },
+      { status: 200 }
+    );
   } catch (error: any) {
     return new NextResponse(`${error.message}: ${error.statusText}`, {
       status: error.status,
--- lib/db/getUsers.ts
+++
@@ -1,8 +0,0 @@
-import { db } from '.';
-import { users } from './schema';
-
-export async function getUsers() {
-  const selectResult = await db.select().from(users);
-  console.log("🚀 ~ getUsers ~ selectResult:", selectResult)
-  return selectResult;
-}
