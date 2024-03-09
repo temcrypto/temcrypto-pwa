a053f4037f49cfe6c55691af929dd7e7b945c6ff
temcryptodev <info@temcrypto.com>
2024-03-08 23:27:12 -0300

    update input butons

--- components/PixKeyInput.tsx
+++ components/PixKeyInput.tsx
@@ -67,28 +67,30 @@ const PixKeyInput = ({
   }, [isClipboardAPISupported]);
 
   return (
-    <>
-      <div className="relative rounded-2xl shadow-sm text-xl">
-        <input
-          type="text"
-          className="transition ease-in-out block w-full rounded-2xl border-0 p-4 pr-20 text-slate-800 placeholder:text-slate-400 ring ring-slate-200 focus:ring-pink-500 focus:outline-none read-only:bg-slate-300 read-only:text-slate-400 read-only:ring-slate-300 appearance-none"
-          readOnly={disabled}
-          ref={inputRef}
-          aria-label="Paste input"
-          {...rest}
-        />
-        {/* <div className="absolute inset-y-0 right-0 flex items-center pr-4 z-50"></div> */}
+    <div className="relative rounded-2xl shadow-sm text-xl">
+      <input
+        type="text"
+        className="transition ease-in-out block w-full rounded-2xl border-0 p-4 pr-20 text-slate-800 placeholder:text-slate-400 ring ring-slate-200 focus:ring-pink-500 focus:outline-none read-only:bg-slate-300 read-only:text-slate-400 read-only:ring-slate-300 appearance-none"
+        readOnly={disabled}
+        ref={inputRef}
+        aria-label="Paste input"
+        {...rest}
+      />
+      <div className="absolute inset-y-0 right-0 flex items-center pr-4 z-50">
+        <button
+          type="button"
+          className="text-pink-500 hover:text-pink-700 disabled:text-slate-400 text-base uppercase"
+          onClick={(e) => {
+            e.preventDefault();
+            handlePaste();
+          }}
+          disabled={disabled}
+          aria-label="Paste button"
+        >
+          Paste
+        </button>
       </div>
-      <button
-        type="button"
-        className="text-pink-500 hover:text-pink-700 disabled:text-slate-400 text-base uppercase"
-        onClick={handlePaste}
-        disabled={disabled}
-        aria-label="Paste button"
-      >
-        Paste
-      </button>
-    </>
+    </div>
   );
 };
 
