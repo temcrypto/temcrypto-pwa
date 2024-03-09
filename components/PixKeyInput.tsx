e4cfbdfe52d10f8df98b99838ff1a91738e16bfe
temcryptodev <info@temcrypto.com>
2024-03-08 23:08:59 -0300

    update input butons

--- components/PixKeyInput.tsx
+++ components/PixKeyInput.tsx
@@ -34,7 +34,7 @@ const PixKeyInput = ({
       clipboardText !== ''
     );
     if (inputRef.current && clipboardText && clipboardText !== '') {
-      console.log("🚀 ~ useEffect ~ clipboardText:", clipboardText)
+      console.log('🚀 ~ useEffect ~ clipboardText:', clipboardText);
       inputRef.current.value = clipboardText;
       // setClipboardText('');
       onPasteSuccess?.();
@@ -67,27 +67,28 @@ const PixKeyInput = ({
   }, [isClipboardAPISupported]);
 
   return (
-    <div className="relative rounded-2xl shadow-sm text-xl">
-      <input
-        type="text"
-        className="transition ease-in-out block w-full rounded-2xl border-0 p-4 pr-20 text-slate-800 placeholder:text-slate-400 ring ring-slate-200 focus:ring-pink-500 focus:outline-none read-only:bg-slate-300 read-only:text-slate-400 read-only:ring-slate-300 appearance-none"
-        readOnly={disabled}
-        ref={inputRef}
-        aria-label="Paste input"
-        {...rest}
-      />
-      <div className="absolute inset-y-0 right-0 flex items-center pr-4 z-50">
-        <div
-          // type="button"
-          className="text-pink-500 hover:text-pink-700 disabled:text-slate-400 text-base uppercase"
-          onClick={handlePaste}
-          // disabled={disabled}
-          aria-label="Paste button"
-        >
-          Paste
-        </div>
+    <>
+      <div className="relative rounded-2xl shadow-sm text-xl">
+        <input
+          type="text"
+          className="transition ease-in-out block w-full rounded-2xl border-0 p-4 pr-20 text-slate-800 placeholder:text-slate-400 ring ring-slate-200 focus:ring-pink-500 focus:outline-none read-only:bg-slate-300 read-only:text-slate-400 read-only:ring-slate-300 appearance-none"
+          readOnly={disabled}
+          ref={inputRef}
+          aria-label="Paste input"
+          {...rest}
+        />
+        {/* <div className="absolute inset-y-0 right-0 flex items-center pr-4 z-50"></div> */}
       </div>
-    </div>
+      <button
+        type="button"
+        className="text-pink-500 hover:text-pink-700 disabled:text-slate-400 text-base uppercase"
+        onClick={handlePaste}
+        disabled={disabled}
+        aria-label="Paste button"
+      >
+        Paste
+      </button>
+    </>
   );
 };
 
