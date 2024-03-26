fa49a316554c87b7091685340cd5aee7049e70b5
temcryptodev <info@temcrypto.com>
2024-03-26 00:48:24 -0300

    comment unused component

--- components/AmountInput.tsx
+++ components/AmountInput.tsx
@@ -1,62 +1,62 @@
-'use client';
+// 'use client';
 
-import {
-  // forwardRef,
-  useCallback,
-  useEffect,
-  // useImperativeHandle,
-  useRef,
-} from 'react';
+// import {
+//   // forwardRef,
+//   useCallback,
+//   useEffect,
+//   // useImperativeHandle,
+//   useRef,
+// } from 'react';
 
 // interface AmountInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 //   onFocus?: () => void;
 // }
 
 // const AmountInput = ({ value, readOnly, disabled, onFocus, ...rest }) => {
-    const amountRef = useRef<HTMLInputElement>(null);
+//     const amountRef = useRef<HTMLInputElement>(null);
 
-    // useImperativeHandle(ref, () => amountRef.current!, []);
+//     // useImperativeHandle(ref, () => amountRef.current!, []);
 
-    const focusInput = useCallback(() => {
-      if (amountRef.current && !readOnly) {
-        amountRef.current.focus();
-        onFocus?.();
-      }
-    }, [readOnly, onFocus]);
+//     const focusInput = useCallback(() => {
+//       if (amountRef.current && !readOnly) {
+//         amountRef.current.focus();
+//         onFocus?.();
+//       }
+//     }, [readOnly, onFocus]);
 
-    useEffect(() => {
-      focusInput();
-    }, [focusInput]);
+//     useEffect(() => {
+//       focusInput();
+//     }, [focusInput]);
 
-    return (
-      <div className="relative mt-8 rounded-2xl shadow-sm text-xl">
-        <input
-          type="text"
-          inputMode="decimal"
-          ref={amountRef}
-          className="transition ease-in-out block w-full rounded-2xl border-0 py-4 pl-12 pr-4 text-slate-800 placeholder:text-slate-400 ring ring-slate-200 focus:ring-pink-500 focus:outline-none appearance-none read-only:ring-slate-300 read-only:focus:ring-slate-300 read-only:bg-slate-300 read-only:text-slate-400 invalid:focus:ring-red-500 invalid:focus:text-red-500 invalid:focus:bg-red-100 peer"
-          readOnly={readOnly || disabled}
-          value={
-            value
-              ? value.toLocaleString('pt-BR', {
-                  minimumFractionDigits: 2,
-                  maximumFractionDigits: 2,
-                })
-              : ''
-          }
-          {...rest}
-        />
-        <div
-          className={
-            'transition ease-in-out pointer-events-none absolute inset-y-0 left-0 flex items-center pl-4 text-slate-800  peer-invalid:text-slate-400 peer-invalid:peer-focus:text-red-500 peer-read-only:text-slate-400'
-          }
-        >
-          R$
-        </div>
-      </div>
-    );
-  }
-);
+//     return (
+//       <div className="relative mt-8 rounded-2xl shadow-sm text-xl">
+//         <input
+//           type="text"
+//           inputMode="decimal"
+//           ref={amountRef}
+//           className="transition ease-in-out block w-full rounded-2xl border-0 py-4 pl-12 pr-4 text-slate-800 placeholder:text-slate-400 ring ring-slate-200 focus:ring-pink-500 focus:outline-none appearance-none read-only:ring-slate-300 read-only:focus:ring-slate-300 read-only:bg-slate-300 read-only:text-slate-400 invalid:focus:ring-red-500 invalid:focus:text-red-500 invalid:focus:bg-red-100 peer"
+//           readOnly={readOnly || disabled}
+//           value={
+//             value
+//               ? value.toLocaleString('pt-BR', {
+//                   minimumFractionDigits: 2,
+//                   maximumFractionDigits: 2,
+//                 })
+//               : ''
+//           }
+//           {...rest}
+//         />
+//         <div
+//           className={
+//             'transition ease-in-out pointer-events-none absolute inset-y-0 left-0 flex items-center pl-4 text-slate-800  peer-invalid:text-slate-400 peer-invalid:peer-focus:text-red-500 peer-read-only:text-slate-400'
+//           }
+//         >
+//           R$
+//         </div>
+//       </div>
+//     );
+//   }
+// );
 
 // AmountInput.displayName = 'AmountInput';
 
