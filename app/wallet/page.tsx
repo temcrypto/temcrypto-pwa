ec8a28267757c9ee11e568880b03e192c3a14d97
temcryptodev <info@temcrypto.com>
2024-04-26 22:44:06 -0300

    add home actions component and wallet export option

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -3,16 +3,20 @@
 'use client';
 
 import toast from 'react-hot-toast';
+import { LuFileKey } from 'react-icons/lu';
 
 import PageWrapper from '@/components/PageWrapper';
 import useCopyToClipboard from '@/hooks/useCopyToClipboard';
-import { useDynamicContext } from '@/lib/dynamicxyz';
+import { useDynamicContext, useEmbeddedReveal } from '@/lib/dynamicxyz';
 import shortenAddress from '@/utils/shortenAddress';
 
 export default function Wallet() {
   const { primaryWallet, user, handleLogOut } = useDynamicContext();
   const { copyToClipboard } = useCopyToClipboard();
+  const { initExportProcess } = useEmbeddedReveal();
 
+  // Set the wallet address to the user's primary wallet if they have one
+  // Otherwise, set it to their email
   const userWallet = primaryWallet?.address ?? '';
   const userWalletType =
     (user?.isAuthenticatedWithAWallet ? user.wallet : 'signinwithemail') ?? '';
@@ -28,23 +32,30 @@ export default function Wallet() {
           <div className="text-md text-slate-400 font-light uppercase mb-3">
             Wallet
           </div>
-          <div className="flex dark:text-white break-words items-center">
-            <img
-              src={`https://iconic.dynamic-static-assets.com/icons/sprite.svg#polygon`}
-              alt={userWallet}
-              height={20}
-              width={20}
-              className="inline me-2"
-            />
-            <button
-              onClick={() => {
-                copyToClipboard(userWallet);
-                toast.success('Copied!');
-              }}
-              className="transition active:text-slate-300 active:scale-95"
-            >
-              {shortenAddress(userWallet)}
-            </button>
+          <div className="flex flex-col space-y-6 dark:text-white break-words">
+            <div className="flex item-center">
+              <img
+                src={`https://iconic.dynamic-static-assets.com/icons/sprite.svg#polygon`}
+                alt={userWallet}
+                height={20}
+                width={20}
+                className="inline me-2"
+              />
+              <button
+                onClick={() => {
+                  copyToClipboard(userWallet);
+                  toast.success('Address copied!');
+                }}
+                className="transition active:text-slate-300 active:scale-95"
+              >
+                {shortenAddress(userWallet)}
+              </button>
+            </div>
+
+            <div className="flex item-center">
+              <LuFileKey className="inline me-2 items-center text-rose-500 w-5 h-5" />
+              <button onClick={() => initExportProcess()}>Export</button>
+            </div>
           </div>
 
           <div className="text-md text-slate-400 font-light uppercase mb-3 mt-6">
--- components/HomeActions.tsx
+++ components/HomeActions.tsx
@@ -1,11 +1,9 @@
 export default function HomeActions() {
   return (
-    <div className="flex rounded-2xl h-14 space-x-1 p-1 border-2 border-pink-500 text-center items-center">
+    <div className="flex rounded-2xl h-14 space-x-1 p-1 border-2 border-rose-500 text-center items-center">
       <div className="flex-1 rounded-l-xl transition active:scale-95">Pay</div>
       <div className="flex-1 transition active:scale-95">Deposit</div>
-      <div className="flex-1 rounded-r-xl transition active:scale-95">
-        Retire
-      </div>
+      <div className="flex-1 rounded-r-xl transition active:scale-95">Send</div>
     </div>
   );
 }
