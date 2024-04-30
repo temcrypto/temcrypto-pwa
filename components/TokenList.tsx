a970da8ac24342d4ea71a8844a4dfdac7dcefd83
temcryptodev <info@temcrypto.com>
2024-04-29 21:22:07 -0300

    added tokens assets and update token list component

--- components/TokenList.tsx
+++ components/TokenList.tsx
@@ -18,6 +18,19 @@ const allowedTokenList = [
     },
   },
   {
+    chainId: 137,
+    name: 'Ether',
+    symbol: 'ETH',
+    decimals: 18,
+    address: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
+    logoURI: 'https://assets.polygon.technology/tokenAssets/eth.svg',
+    tags: ['pos', 'erc20', 'swapable', 'metaTx'],
+    extensions: {
+      originTokenAddress: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+      originTokenNetwork: 0,
+    },
+  },
+  {
     chainId: 1101,
     name: 'Tether USD',
     symbol: 'USDT',
@@ -31,6 +44,38 @@ const allowedTokenList = [
       wrappedTokenNetwork: 1,
     },
   },
+  {
+    chainId: 1101,
+    name: 'USD Coin',
+    symbol: 'USDC.e',
+    decimals: 6,
+    address: '0x37eAA0eF3549a5Bb7D431be78a3D99BD360d19e5',
+    logoURI: 'https://assets.polygon.technology/tokenAssets/usdc.svg',
+    tags: ['lxly', 'stablecoin', 'erc20', 'customZkevmBridge'],
+    extensions: {
+      originTokenAddress: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+      originChainBridgeAdapter: '0x70E70e58ed7B1Cec0D8ef7464072ED8A52d755eB',
+      wrapperChainBridgeAdapter: '0xBDa0B27f93B2FD3f076725b89cf02e48609bC189',
+      originTokenNetwork: 0,
+      wrappedTokenNetwork: 1,
+    },
+  },
+  {
+    chainId: 1101,
+    name: 'Dai',
+    symbol: 'DAI',
+    decimals: 18,
+    address: '0x744C5860ba161b5316F7E80D9Ec415e2727e5bD5',
+    logoURI: 'https://assets.polygon.technology/tokenAssets/dai.svg',
+    tags: ['lxly', 'stablecoin', 'erc20', 'customZkevmBridge'],
+    extensions: {
+      originTokenAddress: '0x6b175474e89094c44da98b954eedeac495271d0f',
+      originChainBridgeAdapter: '0x4A27aC91c5cD3768F140ECabDe3FC2B2d92eDb98',
+      wrapperChainBridgeAdapter: '0x744C5860ba161b5316F7E80D9Ec415e2727e5bD5',
+      originTokenNetwork: 0,
+      wrappedTokenNetwork: 1,
+    },
+  },
 ];
 
 // Demo Data
@@ -38,14 +83,14 @@ const demoTokenList = [
   {
     name: 'Polygon',
     symbol: 'MATIC',
-    logoURI: 'https://assets.polygon.technology/tokenAssets/matic.svg',
+    logoFile: 'matic.svg',
     amount: 0.01553408,
     amountConverted: 49.15,
   },
   {
     name: 'Tether USD',
     symbol: 'USDT',
-    logoURI: 'https://assets.polygon.technology/tokenAssets/usdt.svg',
+    logoFile: 'usdt.svg',
     amount: 110.15,
     amountConverted: 110.15,
   },
@@ -54,7 +99,7 @@ const demoTokenList = [
 type TokenItemData = {
   name: string;
   symbol: string;
-  logoURI: string;
+  logoFile: string;
   amount: number;
   amountConverted: number;
 };
@@ -62,7 +107,7 @@ type TokenItemData = {
 function TokenItem({
   name,
   symbol,
-  logoURI,
+  logoFile,
   amount,
   amountConverted,
 }: TokenItemData) {
@@ -72,7 +117,7 @@ function TokenItem({
         <div className="token-item-data">
           <div className="flex flex-row">
             <Image
-              src={logoURI}
+              src={`/images/tokens/${logoFile}`}
               alt={name}
               height={35}
               width={35}
@@ -102,7 +147,7 @@ export default function TokenList() {
             key={token.name}
             name={token.name}
             symbol={token.symbol}
-            logoURI={token.logoURI}
+            logoFile={token.logoFile}
             amount={token.amount}
             amountConverted={token.amountConverted}
           />
