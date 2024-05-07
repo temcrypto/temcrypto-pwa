f2784f127d45e7d78aa76c311a93b8a77f23fb05
temcryptodev <info@temcrypto.com>
2024-05-07 19:45:13 -0300

    update loading skeleton for token list

--- components/TokenList.tsx
+++ components/TokenList.tsx
@@ -7,8 +7,21 @@ import { type Rate, useRates } from '@/context/RatesContext';
 import { useDynamicContext } from '@/lib/dynamicxyz';
 import { getTokenBalances, type TokenItemData } from '@/utils/getTokenBalances';
 
-import Loading from './Loading';
 import TokenItem from './TokenItem';
+
+function LoadingSkeleton() {
+  return (
+    <div className="flex flex-col space-y-3 *:bg-slate-200 *:dark:bg-slate-500 *:rounded-3xl *:h-20 *:animate-pulse">
+      <div className="" />
+      <div className="" />
+      <div className="" />
+      <div className="" />
+      <div className="" />
+      <div className="" />
+      <div className="" />
+    </div>
+  );
+}
 
 // Define the TokenList props type
 type TokenListProps = {
@@ -48,9 +61,13 @@ export default function TokenList({ address }: TokenListProps) {
     fetchBalance();
   }, [walletConnector, address]);
 
+  // Render the TokenList component only when the wallet is connected and there are balances
+  if (!walletConnector || tokenBalances.length === 0) {
+    return <LoadingSkeleton />;
+  }
+
   return (
     <div className="flex flex-col space-y-3 *:bg-slate-100 *:dark:bg-slate-700 *:rounded-3xl *:p-4">
-      {!walletConnector && <Loading />}
       {tokenBalances.map((token) => (
         <TokenItem
           key={token.symbol}
