68d0e2629f9a941b3d28f4c5af9f4e57baec5a43
temcryptodev <info@temcrypto.com>
2024-05-10 01:54:32 -0300

    update header logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -43,6 +43,8 @@ export default function Header() {
 
   const showAccount = pathname !== '/start';
   const isPathWithLogo = pathsWithLogo.includes(pathname);
+  const showLogo = isPathWithLogo || !primaryWallet;
+
   const currentPathTitle =
     pathTitles.find((path) => pathname.startsWith(path.prefix))?.title || '';
 
@@ -61,7 +63,7 @@ export default function Header() {
     <header className="w-full flex animate-background bg-[length:_400%_400%] [animation-duration:_8s] bg-gradient-to-r from-pink-500 dark:from-pink-400/85 via-indigo-400 dark:via-indigo-400/55 to-cyan-400 dark:to-cyan-400/55 pb-0">
       <div className="h-16 w-full flex justify-between bg-white dark:bg-slate-800 px-6">
         <div className="flex items-center">
-          {isPathWithLogo ? (
+          {showLogo ? (
             <Logo className="animate-bounce-from-bottom" />
           ) : (
             <div className="flex animate-bounce-from-bottom">
