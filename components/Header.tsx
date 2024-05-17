bb311d5ff0e2b54159b7fc2930c46700d411c2b5
temcryptodev <info@temcrypto.com>
2024-05-17 18:09:37 -0300

    cleanup header styles and html

--- components/Header.tsx
+++ components/Header.tsx
@@ -60,43 +60,41 @@ export default function Header() {
   }, [pathname, router]);
 
   return (
-    <header className="w-full flex animate-background bg-[length:_400%_400%] [animation-duration:_8s] bg-gradient-to-r from-pink-500 dark:from-pink-400/85 via-indigo-400 dark:via-indigo-400/55 to-cyan-400 dark:to-cyan-400/55 pb-0">
-      <div className="h-16 w-full flex justify-between bg-white dark:bg-slate-800 px-6">
-        <div className="flex items-center">
-          {showLogo ? (
-            <Logo className="animate-bounce-from-bottom" />
-          ) : (
-            <div className="flex animate-bounce-from-bottom">
-              <button
-                type="button"
-                className="text-3xl text-pink-500 transition active:scale-95"
-                onClick={handleGoTo}
-              >
-                <IoMdArrowBack />
-              </button>
-              <span className="ml-3 text-2xl">{currentPathTitle}</span>
-            </div>
-          )}
-        </div>
-
-        {showAccount && (
-          <div className="flex items-center">
-            {primaryWallet?.address ? (
-              <Link
-                href="/wallet"
-                aria-label={primaryWallet.address}
-                className="animate-bounce-from-bottom transition active:text-slate-300 active:scale-95"
-              >
-                {shortenAddress(primaryWallet.address, 3)}
-              </Link>
-            ) : (
-              <div className="animate-bounce-from-bottom">
-                <LoadingSkeleton className="w-24 h-5" />
-              </div>
-            )}
+    <header className="h-16 w-full flex justify-between bg-white dark:bg-slate-800 px-6 *:flex *:items-center">
+      <div>
+        {showLogo ? (
+          <Logo className="animate-bounce-from-bottom" />
+        ) : (
+          <div className="flex animate-bounce-from-bottom">
+            <button
+              type="button"
+              className="text-3xl text-pink-500 transition active:scale-95"
+              onClick={handleGoTo}
+            >
+              <IoMdArrowBack />
+            </button>
+            <span className="ml-3 text-2xl">{currentPathTitle}</span>
           </div>
         )}
       </div>
+
+      {showAccount && (
+        <div>
+          {primaryWallet?.address ? (
+            <Link
+              href="/wallet"
+              aria-label={primaryWallet.address}
+              className="animate-bounce-from-bottom transition active:text-slate-300 active:scale-95"
+            >
+              {shortenAddress(primaryWallet.address, 3)}
+            </Link>
+          ) : (
+            <div className="animate-bounce-from-bottom">
+              <LoadingSkeleton className="w-24 h-5" />
+            </div>
+          )}
+        </div>
+      )}
     </header>
   );
 }
