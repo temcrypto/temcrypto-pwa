a389cf39fd89c2b0d005951a2446bfd5161f4017
temcryptodev <info@temcrypto.com>
2024-05-24 12:53:24 -0300

    improve loading skeleton classes

--- components/LoadingSkeleton.tsx
+++ components/LoadingSkeleton.tsx
@@ -1,7 +1,15 @@
-export default function LoadingSkeleton({ className }: { className: string }) {
+import { memo } from 'react';
+
+const LoadingSkeleton = memo(function LoadingSkeleton({
+  className = '',
+}: {
+  className: string;
+}) {
   return (
     <div
       className={`animate-pulse bg-slate-100 dark:bg-slate-700 rounded-md ${className}`}
     />
   );
-}
+});
+
+export default LoadingSkeleton;
