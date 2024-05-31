519c2ded632e7be625513e10f546b5b308f043e2
temcryptodev <info@temcrypto.com>
2024-05-31 18:09:09 -0300

    add a custom lib wagmi to use consistently into the project

---
+++ lib/wagmi.ts
@@ -0,0 +1,28 @@
+import { mainnet, polygon, polygonAmoy, polygonMumbai } from 'wagmi/chains';
+import { createConfig, http } from '@wagmi/core';
+
+export { WagmiProvider, useAccount } from 'wagmi';
+export {
+  getEnsAddress,
+  getEnsAvatar,
+  getEnsName,
+  multicall,
+} from '@wagmi/core';
+
+// Wagmi Provider configuration
+export const wagmiConfig = createConfig({
+  chains: [polygon, polygonAmoy, polygonMumbai],
+  multiInjectedProviderDiscovery: false,
+  transports: {
+    [polygon.id]: http(),
+    [polygonAmoy.id]: http(),
+    [polygonMumbai.id]: http(),
+  },
+});
+
+// Wagmi Config for Mainnet
+export const wagmiConfigMainnet = createConfig({
+  chains: [mainnet],
+  multiInjectedProviderDiscovery: false,
+  transports: { [mainnet.id]: http() },
+});
