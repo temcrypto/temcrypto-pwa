e7672737c394f403424e9b87c3c03685bb6a9ac4
temcryptodev <info@temcrypto.com>
2024-06-07 23:30:38 -0300

    update and cleanup start page

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -1,86 +1,86 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
-import { useSession } from 'next-auth/react';
-// import Lottie from 'lottie-react';
+import { useEffect } from 'react';
+import { useSession, signOut } from 'next-auth/react';
 
 import Loading from '@/components/Loading';
-import PageWrapper from '@/components/PageWrapper';
 import { DynamicConnectButton, useDynamicContext } from '@/lib/dynamicxyz';
-
-// Animations
-// import StartAnimation from '@/lottie/start.json';
 
 export default function StartPage({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  const { status: authStatus } = useSession({ required: false });
-  const { showAuthFlow, isAuthenticated, handleLogOut } = useDynamicContext();
-  const prevAuthenticated = useRef(isAuthenticated);
+  const { status: appAuthStatus } = useSession({ required: false });
+  const {
+    showAuthFlow,
+    isAuthenticated: isWalletAuthenticated,
+    handleLogOut,
+    loadingNetwork,
+  } = useDynamicContext();
 
   // Check for authentication status and redirect to callbackUrl or log out
   useEffect(() => {
-    if (isAuthenticated) {
-      if (authStatus === 'authenticated') {
-        const redirectTo = searchParams.callbackUrl || '/';
-        window.location.href = redirectTo as string;
+    const checkAuth = async () => {
+      if (appAuthStatus === 'authenticated') {
+        if (isWalletAuthenticated) {
+          const redirectTo = searchParams.callbackUrl || '/';
+          window.location.href = redirectTo as string;
+        } else {
+          await signOut({ redirect: true, callbackUrl: '/start' });
+        }
       }
+    };
 
-      if (authStatus === 'unauthenticated' && prevAuthenticated.current) {
-        handleLogOut();
-      }
-    }
-  }, [isAuthenticated, authStatus, searchParams, handleLogOut]);
+    checkAuth();
+  }, [isWalletAuthenticated, appAuthStatus, searchParams, handleLogOut]);
 
   return (
-    <PageWrapper id="page-start" className="flex h-full">
-      <div className="flex w-full flex-col">
-        <div className="flex grow items-center text-left">
-          <div className="w-full px-2">
-            {/* <div className="m-auto w-40">
-              <Lottie animationData={StartAnimation} loop={true} />
-            </div> */}
-            <h1 className="mt-20 text-4xl font-extrabold">
-              <span className="text-sky-500">Start</span> living in crypto{' '}
-              <span className="text-rose-500">with</span>{' '}
-              <span className="text-amber-500">us</span>!
-            </h1>
-            <h2 className="mt-8 text-pretty text-2xl text-slate-300 dark:text-slate-400">
-              Explore where digital freedom meets real-world ease.
-            </h2>
-            <h3 className="mb-14 mt-16 animate-background bg-gradient-to-r from-pink-400 via-indigo-400 to-cyan-400 bg-[length:_400%_400%] bg-clip-text text-center text-sm uppercase text-transparent [animation-duration:_4s]">
-              <div className="mt-6">Smart • Gasless • Non-Custodial</div>
-            </h3>
-          </div>
-        </div>
-
-        <div className="flex-none text-center">
-          {isAuthenticated && !showAuthFlow ? (
-            <div className="flex items-center justify-center p-4 text-xl">
-              <Loading bounce={true} />
-            </div>
-          ) : (
-            <>
-              <DynamicConnectButton buttonClassName="w-full justify-center items-center p-4 text-xl text-white bg-pink-500 active:bg-pink-700 rounded-3xl">
-                Continue
-              </DynamicConnectButton>
-              <p className="safe-m-bottom text-pretty py-6 text-sm text-slate-300 dark:text-slate-500">
-                By proceeding, you agree to TEMCRYPTO&apos;s{' '}
-                <a
-                  href="https://temcrypto.com/terms"
-                  className="underline"
-                  target="_blank"
-                >
-                  Terms of Use
-                </a>
-                .
-              </p>
-            </>
-          )}
+    <div className="flex h-full animate-bounce-from-bottom flex-col">
+      <div className="flex grow items-center text-left">
+        <div className="my-10">
+          <h1 className="text-4xl font-extrabold">
+            <span className="text-sky-500">Start</span> living in crypto{' '}
+            <span className="text-rose-500">with</span>{' '}
+            <span className="text-amber-500">us</span>!
+          </h1>
+          <h2 className="mt-8 text-pretty text-2xl text-slate-300 dark:text-slate-400">
+            Explore where digital freedom meets real-world ease.
+          </h2>
+          <h3 className="mt-16 animate-background bg-gradient-to-r from-pink-400 via-indigo-400 to-cyan-400 bg-[length:_400%_400%] bg-clip-text text-center text-sm uppercase text-transparent [animation-duration:_4s]">
+            Smart • Gasless • Non-Custodial
+          </h3>
         </div>
       </div>
-    </PageWrapper>
+
+      <div className="flex h-32 flex-none flex-col items-center text-center">
+        {(isWalletAuthenticated && !showAuthFlow) ||
+        (!isWalletAuthenticated && loadingNetwork) ? (
+          <div className="flex h-full items-center justify-center text-xl">
+            <Loading bounce={true} />
+          </div>
+        ) : (
+          <>
+            <DynamicConnectButton
+              buttonContainerClassName="w-full animate-bounce-from-bottom"
+              buttonClassName="flex w-full justify-center items-center p-4 text-xl text-white bg-pink-500 active:bg-pink-700 rounded-3xl"
+            >
+              Continue
+            </DynamicConnectButton>
+            <p className="text-pretty pt-4 text-sm text-slate-300 dark:text-slate-500">
+              By proceeding, you agree to TEMCRYPTO&apos;s{' '}
+              <a
+                href="https://temcrypto.com/terms"
+                className="underline"
+                target="_blank"
+              >
+                Terms of Use
+              </a>
+              .
+            </p>
+          </>
+        )}
+      </div>
+    </div>
   );
 }
