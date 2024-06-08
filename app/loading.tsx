48627ac8d8b7fc9dbbe09a38a36d11b70986584d
temcryptodev <info@temcrypto.com>
2024-06-07 23:29:17 -0300

    simplify the loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,5 @@
 import LoadingComponent from '@/components/Loading';
 
 export default function Loading() {
-  return (
-    <main id="loading" className="flex-1">
-      <LoadingComponent bounce={true} fullScreen={true} />
-    </main>
-  );
+  return <LoadingComponent bounce={true} fullScreen={true} />;
 }
